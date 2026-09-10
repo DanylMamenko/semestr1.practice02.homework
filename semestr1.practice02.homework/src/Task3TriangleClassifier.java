acb47ea9b90df02f1d6f33b4db4c837d094fbba9
Danylo Mamenko <allivebeats1@gmail.com>
2026-09-11 01:47:05 +0300

    Update Task3TriangleClassifier.java

--- semestr1.practice02.homework/src/Task3TriangleClassifier.java
+++ semestr1.practice02.homework/src/Task3TriangleClassifier.java
@@ -8,7 +8,7 @@ public class Task3TriangleClassifier {
         double c = scanner.nextDouble();
         System.out.println("Сторони трикутника: a= " + a + " b= " + b + " c= " + c);
         boolean exists = (a + b > c) && (a + c > b) && (b + c > a);
-        boolean equalsides = (a == b) && (a == c) && (b == c);
+        boolean equalSides = (a == b) && (a == c) && (b == c);
         boolean equifemoral = (a == b) || (a == c) || (b == c);
         double a2 = Math.pow(a, 2);
         double b2 = Math.pow(b, 2);
@@ -16,7 +16,7 @@ public class Task3TriangleClassifier {
         boolean rectangular = (c2 == a2 + b2) || (a2 == c2 + b2) || (b2 == c2 + a2);
         System.out.println("--------------------------------------------------");
         System.out.println("Чи існує такий трикутник:\t" + exists);
-        System.out.println("Чи є рівностороннім: \t\t" + equalsides);
+        System.out.println("Чи є рівностороннім: \t\t" + equalSides);
         System.out.println("Чи є рівнобедреним:\t\t\t" + equifemoral);
         System.out.println("Чи є прямокутним: \t\t\t" + rectangular);
         scanner.close();
